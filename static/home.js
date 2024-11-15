cefa4494f31673b03edab13a1d79131dba8026d6
randomatoms <77028985+randomatoms@users.noreply.github.com>
2024-11-15 20:23:25 +0000

    Add files via upload

---
+++ static/home.js
@@ -0,0 +1,17 @@
+// JavaScript to toggle dropdown menu
+document.querySelector('.account-icon').addEventListener('click', function() {
+    document.querySelector('.dropdown-content').classList.toggle('show');
+});
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.account-icon')) {
+        var dropdowns = document.getElementsByClassName('dropdown-content');
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+};
